4c9c23cd1fd261a0822ebb3da9237e6792b2bd87
Rich Friedel <rich.friedel@gmail.com>
2024-03-21 23:56:14 -0400

    Add new colors and theme

--- app/src/main/java/net/cursedfunction/deeztasks/ui/home/HomeScreenViewModel.kt
+++ app/src/main/java/net/cursedfunction/deeztasks/ui/home/HomeScreenViewModel.kt
@@ -1,7 +1,29 @@
 package net.cursedfunction.deeztasks.ui.home
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+data class TaskItem(
+    val id: Int,
+    val title: String = "",
+    val description: String = "",
+    val isCompleted: Boolean = false
+)
+
+data class HomeScreenState(
+    val title: String = "",
+    val taskItems: List<TaskItem> = emptyList()
+)
 
 class HomeScreenViewModel : ViewModel() {
+    private val _homeScreenState = MutableStateFlow(HomeScreenState())
+    val homeScreenState: StateFlow<HomeScreenState>
+        get() = _homeScreenState
 
+    init {
+        _homeScreenState.value = HomeScreenState(
+            title = "DeezTasks"
+        )
+    }
 }
