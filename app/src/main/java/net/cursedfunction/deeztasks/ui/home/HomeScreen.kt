73f8e80c0822cb3fff200660e1c1ab85c317b7c1
Rich Friedel <rich.friedel@gmail.com>
2024-03-23 03:07:23 -0400

    Use LazyColumn

--- app/src/main/java/net/cursedfunction/deeztasks/ui/home/HomeScreen.kt
+++ app/src/main/java/net/cursedfunction/deeztasks/ui/home/HomeScreen.kt
@@ -11,8 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Face
@@ -78,7 +78,6 @@ private fun HomeScreen(
     ) { innerPadding ->
         HomeScreenContent(
             modifier = Modifier
-                .fillMaxSize()
                 .padding(innerPadding)
                 .background(color = MaterialTheme.colorScheme.surface),
             screenState = screenState,
@@ -160,92 +159,99 @@ private fun HomeScreenContent(
 ) {
     Column(
         modifier = modifier
-            .padding(horizontal = 16.dp)
-            .verticalScroll(enabled = true, state = rememberScrollState()),
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        screenState.taskItems.forEachIndexed { index, taskItem ->
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp),
-                colors = CardColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
-                    disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
-                ),
-                onClick = {
-                    // TODO: Handle card click
-                    // I like the idea of expanding the card to show more detail
-                    // Maybe we want to expand the card to show the detail?
-                    // If we expand the card, we will need to make it stand out
-                    // visually from the other cards. This could be done by
-                    // slightly increasing the elevation of the card and changing
-                    // the background color. Also, maybe squaring the corners of
-                    // the card a bit. We could also animate the expansion of the
-                    // card to make it more visually appealing.
-                    Log.d("RF43", "Card $index clicked")
-                }
-            ) {
-                Row(
+        val listState = rememberLazyListState()
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            state = listState,
+        ) {
+            items(screenState.taskItems.size) { index ->
+                Card(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(vertical = 16.dp)
-                        .padding(start = 16.dp, end = 4.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Column(
-                        modifier = Modifier.weight(1f),
-                    ) {
-                        Text(
-                            style = MaterialTheme.typography.titleSmall,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            text = "${taskItem.title}: $index"
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text(
-                            style = MaterialTheme.typography.bodyMedium,
-                            maxLines = 2,
-                            overflow = TextOverflow.Ellipsis,
-                            text = "${taskItem.description}: $index"
-                        )
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                    colors = CardColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                        disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    onClick = {
+                        // TODO: Handle card click
+                        // I like the idea of expanding the card to show more detail
+                        // Maybe we want to expand the card to show the detail?
+                        // If we expand the card, we will need to make it stand out
+                        // visually from the other cards. This could be done by
+                        // slightly increasing the elevation of the card and changing
+                        // the background color. Also, maybe squaring the corners of
+                        // the card a bit. We could also animate the expansion of the
+                        // card to make it more visually appealing.
+                        Log.d("RF43", "Card $index clicked")
                     }
-                    Spacer(modifier = Modifier.width(8.dp))
+                ) {
                     Row(
-                        horizontalArrangement = Arrangement.End
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(vertical = 16.dp)
+                            .padding(start = 16.dp, end = 4.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        IconButton(
-                            onClick = { onEditAction(taskItem) }
+                        Column(
+                            modifier = Modifier.weight(1f),
                         ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_pencil),
-                                contentDescription = null
+                            Text(
+                                style = MaterialTheme.typography.titleSmall,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                text = "${screenState.taskItems[index].title}: $index"
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Text(
+                                style = MaterialTheme.typography.bodyMedium,
+                                maxLines = 2,
+                                overflow = TextOverflow.Ellipsis,
+                                text = "${screenState.taskItems[index].description}: $index"
                             )
                         }
-                        IconButton(
-                            onClick = { onDeleteAction(taskItem) }
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Row(
+                            horizontalArrangement = Arrangement.End
                         ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_trash),
-                                contentDescription = null
-                            )
-                        }
-                        IconButton(
-                            onClick = { onCompleteAction(taskItem) }
-                        ) {
-                            Icon(
-                                painter = painterResource(
-                                    id = if (taskItem.isCompleted) {
-                                        R.drawable.ic_check_circle
-                                    } else {
-                                        R.drawable.ic_check_circle_filled
-                                    }
-                                ),
-                                contentDescription = null
-                            )
+                            IconButton(
+                                onClick = { onEditAction(screenState.taskItems[index]) }
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_pencil),
+                                    contentDescription = null
+                                )
+                            }
+                            IconButton(
+                                onClick = { onDeleteAction(screenState.taskItems[index]) }
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_trash),
+                                    contentDescription = null
+                                )
+                            }
+                            IconButton(
+                                onClick = { onCompleteAction(screenState.taskItems[index]) }
+                            ) {
+                                Icon(
+                                    painter = painterResource(
+                                        id = if (screenState.taskItems[index].isCompleted) {
+                                            R.drawable.ic_check_circle
+                                        } else {
+                                            R.drawable.ic_check_circle_filled
+                                        }
+                                    ),
+                                    contentDescription = null
+                                )
+                            }
                         }
                     }
                 }
